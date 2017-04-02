3a440f4edcf3cbe4f90276835d12a3ffd8edece1
Omer Qadir <koder.omer@gmail.com>
2017-04-03 00:28:04 +0200

    renamed scanner.py to ScanDelegate.py

--- src/ScanDelegate.py
+++ src/ScanDelegate.py
@@ -10,10 +10,12 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print "Received new data from", dev.addr
 
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(10.0)
+if __name__ == '__main__':
 
-for dev in devices:
-    print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
-    for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
+    scanner = Scanner().withDelegate(ScanDelegate())
+    devices = scanner.scan(10.0)
+
+    for dev in devices:
+        print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+        for (adtype, desc, value) in dev.getScanData():
+            print "  %s = %s" % (desc, value)
